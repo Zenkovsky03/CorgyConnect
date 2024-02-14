968fef63db2e5477b2cc2bc25a30d37e3907c5c7
Matthew <mateuszzaniewski1@gmail.com>
2024-02-14 12:22:05 +0100

    next test

--- doggs/models.py
+++ doggs/models.py
@@ -3,8 +3,6 @@ import uuid
 from django.db import models
 
 
-# Create your models here.
-
 class Dog(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, editable=False)
     name = models.CharField(max_length=200)
