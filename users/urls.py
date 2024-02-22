253696e18e8a53b588600f565449f24e69992d28
Matthew <mateuszzaniewski1@gmail.com>
2024-02-22 22:26:59 +0100

    add: user account edit functionality(edit personal information)

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
 
     path('login/', views.loginUser, name='login'),
     path('logout/', views.logoutUser, name='logout'),
-    path('register/', views.registerUser, name='register')
+    path('register/', views.registerUser, name='register'),
+
+    path('edit-account/', views.editAccount, name='edit-account'),
 ]
--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, ProfileForm
 def loginUser(request):
     page = 'login'
     if request.user.is_authenticated:
@@ -52,7 +52,7 @@ def registerUser(request):
             user.save()
             messages.success(request, "User Account was created successfully")
             login(request, user)
-            return redirect('profiles')
+            return redirect('edit-account')
         else:
             messages.error(request,"An error has occurred during registration")
     context = {
@@ -83,3 +83,16 @@ def userAccount(request):
         'dogs':dogs,
     }
     return render(request, 'users/account.html', context)
+@login_required(login_url='login')
+def editAccount(request):
+    profile = request.user.profile
+    form = ProfileForm(instance=profile)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('account')
+    context = {
+        'form': form,
+    }
+    return render(request, 'users/profile-form.html', context)
