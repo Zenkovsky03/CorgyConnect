908d904ff74b22e4325c0658ddba2486db57976d
Matthew <mateuszzaniewski1@gmail.com>
2024-03-05 11:48:32 +0100

    hotfix: send email functionality, added, ability to write a message without having to log in, blocked sending messages to yourself

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import CustomUserCreationForm, ProfileForm, SkillForm
+from .forms import CustomUserCreationForm, ProfileForm, SkillForm, MessageForm
 from django.db.models import Q
 from .utils import searchProfiles, paginateProfiles
 def loginUser(request):
@@ -165,3 +165,30 @@ def viewMessage(request, pk):
         'message': message,
     }
     return render(request, 'users/message.html', context)
+
+def createMessage(request, pk):
+    recipient = Profile.objects.get(id=pk)
+    form = MessageForm()
+    try:
+        sender = request.user.profile
+    except:
+        sender = None
+
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            # sender = none
+            message = form.save(commit=False)
+            message.sender = sender
+            message.recipient = recipient
+            if sender:
+                message.name = sender.name
+                message.email = sender.email
+            message.save()
+            messages.success(request, 'Your message was sent successfully!')
+            return redirect('user-profile', pk=recipient.id)
+    context = {
+        'recipient': recipient,
+        'form': form,
+    }
+    return render(request, 'users/message-form.html', context)
