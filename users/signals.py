4e13ff915630273c1c3c3da7fa4ecd14d587d84a
Matthew <mateuszzaniewski1@gmail.com>
2024-03-07 20:00:40 +0100

    edit: send welcome emial with create accout smtp core

--- users/signals.py
+++ users/signals.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import Profile
+from django.core.mail import send_mail
+from django.conf import settings
 
 #@receiver(post_save, sender=Profile)
 def createProfile(sender, instance, created, **kwargs):
@@ -13,6 +15,15 @@ def createProfile(sender, instance, created, **kwargs):
             username=user.username,
             email=user.email,
             name=user.first_name,
+        )
+        subject = 'Welcome to CorgyConnect'
+        message = 'Thanks for signing and support our community! '
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently = False,
         )
 
 def updateUser(sender, instance, created ,**kwargs):
