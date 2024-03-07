9dbb7e4770eb36a5e38d54e11fb304e8b1a94a5f
Matthew <mateuszzaniewski1@gmail.com>
2024-03-07 20:22:11 +0100

    fix: settings.py

--- CorgyConnect/settings.py
+++ CorgyConnect/settings.py
@@ -122,7 +122,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'corgyconnect@gmail.com'
-EMAIL_HOST_PASSWORD = 'nyjg mqqb tvpu uaec'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
