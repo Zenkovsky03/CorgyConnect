dc3f18868c51bbd4de0d9c5fcb74426fa4b380fe
Matthew <mateuszzaniewski1@gmail.com>
2024-03-11 09:03:09 +0100

    add: reset password by email functionality override auth_views

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import PasswordResetView
 from django.forms import ModelForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
