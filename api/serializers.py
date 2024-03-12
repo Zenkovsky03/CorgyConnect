f2c376f08371f82889d255cfd2c3c2e85d31c8af
Matthew <mateuszzaniewski1@gmail.com>
2024-03-12 22:39:58 +0100

    add: new serializers for Review, Tags, Profiles and add SerializerMethod to show dog's reviews

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,41 @@
 from rest_framework import serializers
-from doggs.models import Dog
+from doggs.models import Dog, Tag, Review
+from users.models import Profile
+
+
+
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
 
 class DogSerializer(serializers.ModelSerializer):
+    owner = ProfileSerializer(many=False)
+    tags = TagSerializer(many=True)
+    reviews = serializers.SerializerMethodField()
     class Meta:
         model = Dog
         fields = '__all__'
+
+    def get_reviews(self, obj):
+        reviews = obj.review_set.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return serializer.data
+
+
+
+
