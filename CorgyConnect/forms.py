3eec3e2f0c9d97f0fb15a5610e5c8091b4cfe06e
Matthew <mateuszzaniewski1@gmail.com>
2024-03-14 23:38:01 +0100

    delete: forms.py in aplication core

--- CorgyConnect/forms.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.forms import PasswordResetForm
-class CustomPasswordResetForm(PasswordResetForm):
-    def __init__(self, *args, **kwargs):
-        super(CustomPasswordResetForm, self).__init__(*args, **kwargs)
-
-        for name, field in self.fields.items():
-            field.widget.attrs.update({'class': 'input'})
