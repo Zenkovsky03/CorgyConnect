9d712d7b63f50e4d91405d345640c92930b767e9
Matthew <mateuszzaniewski1@gmail.com>
2024-03-18 18:23:03 +0100

    create: POST request to vote dog

--- api/urls.py
+++ api/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
 
     path('dogs/', views.getDogs),
     path('dogs/<str:pk>/', views.getDog),
+    path('dogs/<str:pk>/vote/', views.dogVote),
 
     path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .serializers import DogSerializer
-from doggs.models import Dog
+from doggs.models import Dog, Review
 @api_view(['GET'])
 def getRoutes(request):
 
@@ -30,4 +30,23 @@ def getDogs(request):
 def getDog(request, pk):
     dog = Dog.objects.get(id=pk)
     serializer = DogSerializer(dog, many=False) #many=True to znaczy ze wiele obiektow a nie tylko jeden
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def dogVote(request, pk):
+    dog = Dog.objects.get(id=pk)
+    user = request.user.profile
+    data = request.data
+    review, created = Review.objects.get_or_create(
+        owner=user,
+        dog=dog,
+    )
+    # czy istnieje recenzja w bazie danych, której właścicielem jest określony użytkownik dla określonego psa.
+    # Jeśli taka recenzja już istnieje, jest ona pobierana. Jeśli nie istnieje, jest tworzona nowa recenzja.
+    # Zmienna created informuje nas, czy recenzja została utworzona podczas tej operacji czy nie
+    review.value = data['value']
+    review.save()
+    dog.getVoteCount
+    serializer = DogSerializer(dog, many=False)
     return Response(serializer.data)
